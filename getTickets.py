51e7f6997733a4637a1933923535a990599e3c19
Baran Azak <74122725+baran61@users.noreply.github.com>
2023-04-14 10:24:17 +0100

    Update getTickets.py

--- getTickets.py
+++ getTickets.py
@@ -5,26 +5,13 @@ import requests
 from database import openDbConnection
 
 
-# def download_database_file():
-#    url = 'https://example.com/'
-#    response = requests.get(url)
-#    with open('LAPTOP-PG8J0ME5\SQLEXPRESS', 'wb') as f:
-#        f.write(response.content)
-#    print('Database file downloaded !')
-
 def createTicket(booking_id):
     cursor = openDbConnection()
-    getTherterQuery = """
-            SELECT Theater.thearerName, Area.areaName, Booking.customerName, Movie.movieName, Movie.showTiming, Booking.seatBooked
-            FROM Booking
-            INNER JOIN Theater ON Booking.theraterId = Theater.theaterId
-            INNER JOIN Area ON Theater.areaId = Area.areaId
-            INNER JOIN Movie ON Booking.movieId = Movie.movieId
-            WHERE Booking.bookingId = ?
-            """
-    cursor.execute(getTherterQuery, (booking_id,))
-    # Fetch results
-    results = cursor.fetchall()
+
+    getTherterQuery = "SELECT t.thearerName, a.areaName, b.customerName, m.movieName, m.showTiming, b.seatBooked FROM [movieDb].[dbo].[Booking] b INNER JOIN [movieDb].[dbo].[Theater] t ON b.theraterId = t.theaterId INNER JOIN [movieDb].[dbo].[Area] a ON t.areaId = a.areaId INNER JOIN [movieDb].[dbo].[Movie] m ON b.movieId = m.movieId WHERE b.bookingId = " + str(
+        booking_id)
+    print(getTherterQuery)
+    results = cursor.execute(getTherterQuery).fetchall()
     with open('bookingdetails.txt', 'w') as f:
         for theater_name, area_name, customer_name, movie_name, show_timing, seat_booked in results:
             f.write("The booking details for booking ID {} are:\n".format(booking_id))
@@ -36,47 +23,10 @@ def createTicket(booking_id):
             f.write("Booked Seats: {}\n".format(seat_booked))
             f.write("\n")
 
+    url = 'file:///C:/Users/baran/OneDrive/Masa%C3%BCst%C3%BC/Python%20Project/bookingdetails.txt'
+    response = requests.get(url)
 
-# try:
-#     # if u have used window authentication for local running DB
-#     dbConnection = pyodbc.connect('Driver={SQL Server};'
-#                                   'Server=LAPTOP-PG8J0ME5\SQLEXPRESS;'
-#                                   'Database=Movie db;'
-#                                   'Trusted_Connection=yes;')
+    with open('bookingdetails.txt', 'w') as f:
+        f.write(response.text)
 
-#     if True:
-#         cursor = dbConnection.cursor()
-#         # Set booking ID to fetch
-#         booking_id = 102
-#         # Execute query with booking ID parameter
-#         cursor.execute("""
-#             SELECT Theater.thearerName, Area.areaName, Booking.customerName, Movie.movieName, Movie.showTiming, Booking.seatBooked
-#             FROM Booking
-#             INNER JOIN Theater ON Booking.theraterId = Theater.theaterId
-#             INNER JOIN Area ON Theater.areaId = Area.areaId
-#             INNER JOIN Movie ON Booking.movieId = Movie.movieId
-#             WHERE Booking.bookingId = ?
-#             """, (booking_id,))
-#         # Fetch results
-#         results = cursor.fetchall()
-
-# except Exception as e:
-#     print("Error while connecting to DB", e)
-
-# # Closing DB
-# finally:
-#     cursor.close()
-#     dbConnection.close()
-#     print("db connection closed")
-
-#     # Write the results to a file
-#     with open('bookingdetails.txt', 'w') as f:
-#         for theater_name, area_name, customer_name, movie_name, show_timing, seat_booked in results:
-#             f.write("The booking details for booking ID {} are:\n".format(booking_id))
-#             f.write("Theater Name: {}\n".format(theater_name))
-#             f.write("Theater Area: {}\n".format(area_name))
-#             f.write("Customer Name: {}\n".format(customer_name))
-#             f.write("Movie Name: {}\n".format(movie_name))
-#             f.write("Show Timing: {}\n".format(show_timing))
-#             f.write("Booked Seats: {}\n".format(seat_booked))
-#             f.write("\n")
+    print('Text file downloaded.')
