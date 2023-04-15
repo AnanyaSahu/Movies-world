0047a5b8a8759f061922edf68ae507dc5ca40f09
snehac2224 <123369309+snehac2224@users.noreply.github.com>
2023-04-15 23:31:49 +0100

    Update movieshows.py

--- movieshows.py
+++ movieshows.py
@@ -1,11 +1,19 @@
-from database import openDbConnection
+from database import databaseConnection
 
-# this method is used to get the list of movies from the selected theater
-def get_movies_by_theatre(theatreId):
-    cursor = openDbConnection()
+
+class getMovieShow:
+  def __init__(self):
+    pass
+      # this method is used to get the list of movies from the selected theater
+  def get_movies_by_theatre(self,theatreId):
+    d = databaseConnection()
+    cursor = d.openDbConnection()
     getMovieByTheaterQuery = "SELECT tm.id, tm.theaterId ,tm.movieId, m.movieName, m.showTiming, m.duration, m.ageConstraint FROM [movieDb].[dbo].[TheaterMovie] tm INNER JOIN [movieDb].[dbo].[Movie] m  on tm.movieId = m.movieId where tm.theaterId = "+ str(theatreId) 
     print(getMovieByTheaterQuery)
     record = cursor.execute(getMovieByTheaterQuery).fetchall()
     print(record)
     r= [tuple(row) for row in record]
     return {'rows': r}
+
+
+     
