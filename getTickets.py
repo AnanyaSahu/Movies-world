da7184d0b578a961eb305c1d386e3c50b151e54e
Baran Azak <74122725+baran61@users.noreply.github.com>
2023-04-16 00:59:41 +0100

    Update getTickets.py

--- getTickets.py
+++ getTickets.py
@@ -1,32 +1,69 @@
 # Baran Azak
 # commited changes
-import pyodbc
-import requests
-from database import openDbConnection
+from database import databaseConnection
+from pathlib import Path
+import os
+from flask import Flask, send_file
 
 
-def createTicket(booking_id):
-    cursor = openDbConnection()
+class createTicketsForBookings:
+    def _init_(self):
+        pass
 
-    getTherterQuery = "SELECT t.thearerName, a.areaName, b.customerName, m.movieName, m.showTiming, b.seatBooked FROM [movieDb].[dbo].[Booking] b INNER JOIN [movieDb].[dbo].[Theater] t ON b.theraterId = t.theaterId INNER JOIN [movieDb].[dbo].[Area] a ON t.areaId = a.areaId INNER JOIN [movieDb].[dbo].[Movie] m ON b.movieId = m.movieId WHERE b.bookingId = " + str(
-        booking_id)
-    print(getTherterQuery)
-    results = cursor.execute(getTherterQuery).fetchall()
-    with open('bookingdetails.txt', 'w') as f:
-        for theater_name, area_name, customer_name, movie_name, show_timing, seat_booked in results:
-            f.write("The booking details for booking ID {} are:\n".format(booking_id))
-            f.write("Theater Name: {}\n".format(theater_name))
-            f.write("Theater Area: {}\n".format(area_name))
-            f.write("Customer Name: {}\n".format(customer_name))
-            f.write("Movie Name: {}\n".format(movie_name))
-            f.write("Show Timing: {}\n".format(show_timing))
-            f.write("Booked Seats: {}\n".format(seat_booked))
-            f.write("\n")
+    def createTicket(self,booking_id):
+        d = databaseConnection()
+        cursor = d.openDbConnection()
 
-    url = 'file:///C:/Users/baran/OneDrive/Masa%C3%BCst%C3%BC/Python%20Project/bookingdetails.txt'
-    response = requests.get(url)
+        getTherterQuery = "SELECT t.theaterId , t.thearerName, a.areaName, b.customerName, m.movieName, m.showTiming, b.seatBooked FROM [movieDb].[dbo].[Booking] b INNER JOIN [movieDb].[dbo].[Theater] t ON b.theraterId = t.theaterId INNER JOIN [movieDb].[dbo].[Area] a ON t.areaId = a.areaId INNER JOIN [movieDb].[dbo].[Movie] m ON b.movieId = m.movieId WHERE b.bookingId = " + str(
+            booking_id)
+        print(getTherterQuery)
+        results = cursor.execute(getTherterQuery).fetchall()
+        r= [tuple(row) for row in results]
+        # with open('bookingdetails.txt', 'w') as f:
+            # for theater_name, area_name, customer_name, movie_name, show_timing, seat_booked in results:
+                # f.write("The booking details for booking ID {} are:\n".format(results[0][0]))
+                # f.write("Theater Name: {}\n".format(results[0][1]))
+                # f.write("Theater Area: {}\n".format(results[0][2]))
+                # f.write("Customer Name: {}\n".format(results[0][3]))
+                # f.write("Movie Name: {}\n".format(results[0][4]))
+                # f.write("Show Timing: {}\n".format(results[0][5]))
+                # f.write("Booked Seats: {}\n".format(results[0][6]))
+                # f.write("\n")
+        return {'rows': r}
 
-    with open('bookingdetails.txt', 'w') as f:
-        f.write(response.text)
+        # url = './bookingdetails.txt'
+        # response = requests.get(url)
 
-    print('Text file downloaded.')
+        # # # with open('./bookingdetails.txt', 'w') as f:
+        # # #     f.write(response.text)
+        # filename = Path('bookingdetails.txt')
+        # print(filename)
+        # open(filename, 'wb').write(bookingdetails.c)
+        
+        # print('Text file downloaded.')
+
+    # def downlaodTicket(booking_id):
+    #     cursor = openDbConnection()
+
+    #     getTherterQuery = "SELECT t.theaterId , t.thearerName, a.areaName, b.customerName, m.movieName, m.showTiming, b.seatBooked FROM [movieDb].[dbo].[Booking] b INNER JOIN [movieDb].[dbo].[Theater] t ON b.theraterId = t.theaterId INNER JOIN [movieDb].[dbo].[Area] a ON t.areaId = a.areaId INNER JOIN [movieDb].[dbo].[Movie] m ON b.movieId = m.movieId WHERE b.bookingId = " + str(
+    #         booking_id)
+    #     results = cursor.execute(getTherterQuery).fetchall()
+    #     r= [tuple(row) for row in results]
+    #     with open('bookingdetails.txt', 'w') as f:
+    #             f.write("The booking details for booking ID {} are:\n".format(results[0][0]))
+    #             f.write("Theater Name: {}\n".format(results[0][1]))
+    #             f.write("Theater Area: {}\n".format(results[0][2]))
+    #             f.write("Customer Name: {}\n".format(results[0][3]))
+    #             f.write("Movie Name: {}\n".format(results[0][4]))
+    #             f.write("Show Timing: {}\n".format(results[0][5]))
+    #             f.write("Booked Seats: {}\n".format(results[0][6]))
+    #             f.write("\n")
+    
+
+    #     filepath = os.path.join(os.getcwd(), 'bookingdetails.txt')
+
+    #     # Download the file
+    #     return send_file(filepath, as_attachment=True)
+
+
+    # downlaodTicket(168)
